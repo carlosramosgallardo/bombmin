2fd9b6e453c0f6e574d8d29151a873d8cedf735a
crg <botsandpods@gmail.com>
2025-03-26 14:47:21 +0100

    Remove not-found component

--- app/not-found.js
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl">Page Not Found (404)</h1>
-    </div>
-  );
-}
