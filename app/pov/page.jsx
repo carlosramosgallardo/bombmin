5ce66cb0fc0576e45c04307346301d3dbd23cd6f
crg <botsandpods@gmail.com>
2025-03-26 23:45:45 +0100

    fix pov

--- app/pov/page.jsx
+++ app/pov/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { WagmiConfig, createConfig, http, useAccount } from 'wagmi';
 import { mainnet } from 'wagmi/chains';
