b928529656fbcb6045145dc15ecb46f77c919c94
crg <botsandpods@gmail.com>
2025-03-26 13:24:01 +0100

    Add custom 404 page for NotFound route

---
+++ app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl">Page Not Found (404)</h1>
+    </div>
+  );
+}
