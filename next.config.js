b7881711eb680365938e0d44d30dd315ee1d6f45
crg <botsandpods@gmail.com>
2025-03-26 14:17:05 +0100

    Fix not-found page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-// next.config.js
 module.exports = {
   reactStrictMode: true,
+};
+
+# app/not-found.js
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl">Page Not Found (404)</h1>
+    </div>
+  );
 }
+
