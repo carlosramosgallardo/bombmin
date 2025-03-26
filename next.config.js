a05b21d74f47401dec1ecec90c1522422d985322
crg <botsandpods@gmail.com>
2025-03-26 14:22:13 +0100

    Fix: Separate config and custom 404 page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
 };
 
-# app/not-found.js
-export default function NotFound() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-3xl">Page Not Found (404)</h1>
-    </div>
-  );
-}
+module.exports = nextConfig;
 
