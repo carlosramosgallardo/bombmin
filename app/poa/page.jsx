381e1b85220fa30a77882c508056ce4420f2942f
crg <botsandpods@gmail.com>
2025-04-01 18:07:47 +0200

    fix

--- app/poa/page.jsx
+++ app/poa/page.jsx
@@ -95,7 +95,7 @@ function PoAClientComponent() {
   };
 
   return (
-    <main className="flex flex-col items-center w-full px-4 pt-10 pb-20 text-sm font-mono text-gray-200 bg-black">
+    <main className="flex flex-col items-center w-full px-4 pt-10 pb-20 text-sm font-mono text-gray-200 bg-black min-h-screen">
       <div className="max-w-3xl w-full">
         <h1 className="text-3xl font-bold mb-6">Proof of Ask</h1>
 
