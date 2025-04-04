09d744da46e4de8da8197fc54f2ea35c0c915078
crg <botsandpods@gmail.com>
2025-04-04 19:25:57 +0200

    images

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -14,10 +14,10 @@ export default function NavLinks({ className = '' }) {
     <div className={`text-base text-gray-400 space-x-3 ${className}`}>
       <Link href="/theory" className={linkClass('/theory')}>Math to Mine</Link>
       <Link href="/" className={linkClass('/')}>MM3</Link>
-      <Link href="/manifesto" className={linkClass('/manifesto')}>Manifesto</Link>
       <Link href="/pov" className={linkClass('/pov')}>Poof of Vote</Link>
       <Link href="/poa" className={linkClass('/poa')}>Proof of Ask</Link>
       <Link href="/api" className={linkClass('/api')}>API</Link>
+      <Link href="/manifesto" className={linkClass('/manifesto')}>Manifesto</Link>
     </div>
   )
 }
