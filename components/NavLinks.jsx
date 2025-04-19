6282db90cf3243c1224defa5094b55b001baa010
crg <botsandpods@gmail.com>
2025-04-19 22:08:06 +0200

    cool

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -14,8 +14,8 @@ export default function NavLinks({ className = '' }) {
     <div className={`flex flex-wrap justify-center text-base text-gray-400 space-x-3 ${className}`}>
       <Link href="/" className={linkClass('/')}>MM3</Link>
       <Link href="/learn-math" className={linkClass('/learn-math')}>Learn Math</Link>
-      <Link href="/pov" className={linkClass('/pov')}>Proof of Vote</Link>
-      <Link href="/poa" className={linkClass('/poa')}>Proof of Ask</Link>
+      <Link href="/pov" className={linkClass('/pov')}>PoV</Link>
+      <Link href="/poa" className={linkClass('/poa')}>PoA</Link>
       <Link href="/api" className={linkClass('/api')}>API</Link>
       <Link href="/manifesto" className={linkClass('/manifesto')}>Manifesto</Link>
       <Link href="/manifesto" className={linkClass('/playlist')}>Playlist</Link>
